9cc6d1c10fda51ed8e8a390950bd22738d83ce15
Alisher <shera.rashiduulu@gmail.com>
2019-09-25 13:34:40 +0600

    Created admin.py and models.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Task, Type, Status
 
-# Register your models here.
+class TodolistAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'summary', 'description', 'status', 'type', 'created_at']
+    list_filter = ['summary']
+    list_display_links = ['pk', 'description']
+    search_fields = ['summary', 'description']
+    fields = ['summary', 'description', 'type', 'status', 'created_at']
+    readonly_fields = ['created_at']
+
+
+
+admin.site.register(Task, TodolistAdmin)
+admin.site.register(Type)
+admin.site.register(Status)
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    summary = models.CharField(max_length=50, null=False, blank=False, verbose_name='summary')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='descripton')
+    status = models.ForeignKey('webapp.Status', related_name='task_status', on_delete=models.PROTECT, verbose_name='status')
+    type = models.ForeignKey('webapp.Type', related_name='task_type', on_delete=models.PROTECT, verbose_name='type')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created_at')
+
+    def __str__(self):
+        return self.summary
+
+class Type(models.Model):
+    type = models.CharField(max_length=50, verbose_name='type')
+
+    def __str__(self):
+        return self.type
+
+
+class Status(models.Model):
+    status = models.CharField(max_length=50, verbose_name='status')
+
+    def __str__(self):
+        return self.status
