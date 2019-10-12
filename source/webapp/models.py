84ba1e3c6859fe49cc8ea72eb199a1026ab0bdb9
Alisher <shera.rashiduulu@gmail.com>
2019-10-12 20:01:10 +0600

    Added search to task_views.py

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -16,26 +16,26 @@ class Project(models.Model):
 
 
 class Task(models.Model):
-    summary = models.CharField(max_length=50, null=False, blank=False, verbose_name='summary')
-    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='descripton')
-    status = models.ForeignKey('webapp.Status', related_name='task_status', on_delete=models.PROTECT, null=True, blank=True, verbose_name='status')
-    type = models.ForeignKey('webapp.Type', related_name='task_type', on_delete=models.PROTECT, null=True, blank=True, verbose_name='type')
-    project = models.ForeignKey('webapp.Project', related_name='projects_task', on_delete=models.PROTECT, null=True, blank=False, verbose_name='project')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created_at')
+    summary = models.CharField(max_length=50, null=False, blank=False, verbose_name='краткое изложение')
+    description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='описание')
+    status = models.ForeignKey('webapp.Status', related_name='task_status', on_delete=models.PROTECT, null=True, blank=True, verbose_name='статус')
+    type = models.ForeignKey('webapp.Type', related_name='task_type', on_delete=models.PROTECT, null=True, blank=True, verbose_name='тип')
+    project = models.ForeignKey('webapp.Project', related_name='projects_task', on_delete=models.PROTECT, null=True, blank=False, verbose_name='проект')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создано')
 
     def __str__(self):
         return self.summary
 
 
 class Type(models.Model):
-    type = models.CharField(max_length=50, verbose_name='type')
+    type = models.CharField(max_length=50, verbose_name='тип')
 
     def __str__(self):
         return self.type
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=50, verbose_name='status')
+    status = models.CharField(max_length=50, verbose_name='статус')
 
     def __str__(self):
         return self.status
--- source/webapp/views/task_views.py
+++ source/webapp/views/task_views.py
@@ -1,6 +1,8 @@
+from django.db.models import Q
 from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
 
-from webapp.forms import TaskForm
+from webapp.forms import TaskForm, SimpleSearchForm
 from webapp.models import Task
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
@@ -14,6 +16,33 @@ class IndexView(ListView):
     paginate_by = 4
     paginate_orphans = 1
 
+
+    def get(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        context['form'] = self.form
+        if self.search_value:
+            context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            query = Q(summary__icontains=self.search_value) | Q(description__icontains=self.search_value)
+            queryset = queryset.filter(query)
+        return queryset
+
+    def get_search_form(self):
+        return SimpleSearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data['search']
+        return None
 
 class TaskView(DetailView):
     template_name = 'task/task.html'
