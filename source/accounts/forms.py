dad68e8f1f80da0eef7d218c25a155d727e1f2d0
Alisher <shera.rashiduulu@gmail.com>
2019-10-27 15:12:11 +0600

    Сложный способ сделать next для перенаправления после логина

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -1,9 +1,38 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class SignUpForm(forms.Form):
     first_name = forms.CharField(max_length=100, required=True, label='First name')
     last_name = forms.CharField(max_length=100, required=True, label='Last name')
     password = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput,
+                               label='Password')
+    password_confirm = forms.CharField(max_length=100, required=True, widget=forms.PasswordInput,
                                label='Password confirm')
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        try:
+            User.objects.get(first_name=first_name)
+            raise ValidationError('This name is already taken', code='taken_name')
+        except User.DoesNotExist:
+            return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+        try:
+            User.objects.get(last_name=last_name)
+            raise ValidationError('This last name is already taken', code='taken_last_name')
+        except User.DoesNotExist:
+            return last_name
+
+    def clean(self):
+        super().clean()
+        password_1 = self.cleaned_data.get('password')
+        password_2 = self.cleaned_data.get('password_confirm')
+        if password_1 != password_2:
+            raise ValidationError('Passwords do not match', code='password_do_not_match')
+        return self.cleaned_data
+
 
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -8,14 +8,21 @@ from accounts.forms import SignUpForm
 
 def login_view(request):
     context = {}
+    if request.method == 'GET':
+        next_url = request.GET.get('next', '')
+        context['next'] = next_url
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        next_url = request.POST.get('next', '')
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if next_url:
+                return redirect(next_url)
             return redirect('webapp:index')
         else:
+            context['next'] = next_url
             context['has_error'] = True
     return render(request, 'login.html', context=context)
 
@@ -39,6 +46,8 @@ def register_view(request):
             )
             user.set_password(form.cleaned_data.get('password'))
             user.save()
+            login(request, user)
+            return redirect('webapp:index')
         else:
             return render(request, 'register.html', context={'form': form})
 
